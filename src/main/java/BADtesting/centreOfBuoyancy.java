a7d6efd1e8eddde1350781fc90c5e990f8340e3b
cxsong1 <csca12.cs@gmail.com>
2020-12-28 19:49:04 -0800

    CSV input finalized

--- src/main/java/BADtesting/centreOfBuoyancy.java
+++ src/main/java/BADtesting/centreOfBuoyancy.java
@@ -1,9 +1,6 @@
 package BADtesting;
 
-import org.w3c.dom.ls.LSOutput;
-
 import java.io.*;
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -17,99 +14,127 @@ import static BADtesting.overallBuoyancy.readLinesFromFile;
 
 public class centreOfBuoyancy {
 
-	static Scanner input = new Scanner(System.in);
-	private static String CSV_PATH = input.next();
-	public static ArrayList<String> aList = new ArrayList<String>();
-
 		public static void main(String[] args) throws IOException{
 
-			readLinesFromFile(CSV_PATH); //should modify aList directly
-			double theta = 0.0, deltaT = 0.0, acceleration = 0.0;
+			//copy the values from the CSV to four different arraylists - one for each column
 
-			ArrayList<Data> dataList = convertToData(aList);
+			//first declare the four arraylists
+			ArrayList<Double> thetaList = new ArrayList<>();
+			ArrayList<Double> deltaTList = new ArrayList<>();
+			ArrayList<Double> accList = new ArrayList<>();
+			//ArrayList<Double> depthList = new ArrayList<>();
 
-			for(int i = 0; i < dataList.size(); i++){
-				theta = dataList.get(i).getTheta();
-				deltaT = dataList.get(i).getDeltaT();
-				acceleration = dataList.get(i).getAcceleration();
+			double theta, deltaT, acceleration;
+			String filePath = "C:\\Users\\cxson\\OneDrive\\Desktop\\Documents\\SUBC\\BADtesting\\src\\main\\java\\BADtesting\\badTest.csv";
+			File file = new File(filePath);
+
+			//read from file
+			Scanner inputStream;
+			{
+				try {
+					inputStream = new Scanner(file);
+					String data = inputStream.next(); //ignore first line
+					while(inputStream.hasNext()){
+						data = inputStream.next(); //data is the entire line
+						String[] values = data.split(",");
+						thetaList.add(Double.parseDouble(values[0]));
+						deltaTList.add(Double.parseDouble(values[1]));
+						accList.add(Double.parseDouble(values[2]));
+						//depthList.add(Double.parseDouble(values[3]));
+
+						System.out.println(values[0]); //this means get all the values in the 3rd column
+					}
+					inputStream.close();
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				}
 			}
 
-			int instruction = 0;
-			int R = 0;
-			int L = 1;
-			int updated;
+			for(int i = 0; i < thetaList.size(); i++) {
 
-			/*
-			encoding that gives the signs of the three parameters
-			Add nothing if any of the parameters = 0
-			1 = positive
-			2 = negative
-			*/
-			int code = 0;
+				theta = thetaList.get(i);
+				deltaT = deltaTList.get(i);
+				acceleration = accList.get(i);
 
-			if (theta > 0.0)
-				code += 100;
-			else if (theta < 0.0)
-				code += 200;
+				int instruction = 0;
+				int R = 0;
+				int L = 1;
+				int updated;
 
-			if (deltaT > 0.0)
-				code += 10;
-			else if (deltaT < 0.0)
-				code += 20;
+				/*
+				encoding that gives the signs of the three parameters
+				Add nothing if any of the parameters = 0
+				1 = positive
+				2 = negative
+				*/
+				int code = 0;
 
-			if (acceleration > 0.0)
-				code += 1;
-			else if (acceleration < 0.0)
-				code += 2;
+				if (theta > 0.0)
+					code += 100;
+				else if (theta < 0.0)
+					code += 200;
 
-			switch(code){
-				//Case 1
-				case 111:
-					instruction = L*10 + 3;
-					break;
-				//Case 3, 12
-				case 112:
-				case 100:
-				case 10:
-					instruction = L*10 + 2;
-					break;
-				//Case 7
-				case 221: instruction = R*10 + 3;
-					break;
-				//Case 8, 11, 13
-				case 222:
-				case 200:
-				case 20:
-					instruction = R*10 + 2;
-					break;
-				//Case 2, 4
-				case 121:
-				case 122:
-					updated = check(instruction, theta, deltaT, acceleration);
-					if(updated>=200 || updated<100) {
-						if (code == 121)
+				if (deltaT > 0.0)
+					code += 10;
+				else if (deltaT < 0.0)
+					code += 20;
+
+				if (acceleration > 0.0)
+					code += 1;
+				else if (acceleration < 0.0)
+					code += 2;
+
+				switch (code) {
+					//Case 1
+					case 111:
+						instruction = L * 10 + 3;
+						break;
+					//Case 3, 12
+					case 112:
+					case 100:
+					case 10:
+						instruction = L * 10 + 2;
+						break;
+					//Case 7
+					case 221:
+						instruction = R * 10 + 3;
+						break;
+					//Case 8, 11, 13
+					case 222:
+					case 200:
+					case 20:
+						instruction = R * 10 + 2;
+						break;
+					//Case 2, 4
+					case 121:
+					case 122:
+						updated = check(instruction, theta, deltaT, acceleration);
+						if (updated >= 200 || updated < 100) {
+							if (code == 121)
+								instruction = R * 10 + 1;
+							else
+								instruction = R * 10 + 2;
+						} else
+							instruction = L * 10 + 1;
+						break;
+					//Case 5, 6 **MAY NEED CHANGING
+					case 211:
+					case 212:
+						updated = check(instruction, theta, deltaT, acceleration);
+						if (updated < 200)
+							if (code == 212)
+								instruction = L * 10 + 1;
+							else
+								instruction = L * 10 + 2;
+						else
 							instruction = R * 10 + 1;
-						else
-							instruction = R * 10 + 2;
-					}else
-						instruction = L*10+1;
-					break;
-				//Case 5, 6 **MAY NEED CHANGING
-				case 211:
-				case 212:
-					updated = check(instruction, theta, deltaT, acceleration);
-					if(updated<200)
-						if(code == 212)
-							instruction = L*10 + 1;
-						else
-							instruction = L*10 + 2;
-					else
-						instruction = R*10 + 1;
-					break;
-				//Case 9
-				default : instruction = 0;
+						break;
+					//Case 9
+					default:
+						instruction = 0;
+				}
+				returnFunc(instruction);
 			}
-			returnFunc(instruction);
 		}
 
 		public static int check(int currInstr, double prevTheta, double prevDelta, double prevAcc)	{
